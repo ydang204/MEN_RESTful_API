b8de892b8a69e61592dfbc955a3d5878091b0801
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-07-05 17:40:25 +0700

    register

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
 const user = require('../models/user');
+const { registerValidation } = require('../validation');
 
 // registration
 router.post("/register", async (req, res) => {
+    //validate the user input (name, email, password)
+    const { error } = registerValidation(req.body);
 
+    if(error) {
+        return res.status(400).json({error: error.detail[0].message });
+    }
+    //check if the email is already registered
+    //hash the password
+    //create a user object and save in the db
+    //return res.status(200).json({ msg: "Register route..." });
 })
 
 //login
 router.post("/login", async (req, res) => {
-
+    return res.status(200).json({ msg: "login route..." });
 })
 
 module.exports = router;
