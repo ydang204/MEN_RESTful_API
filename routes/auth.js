402394fec385e18de8dbb54683e92ccb8d3df7a8
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-07-06 16:57:06 +0700

    Completed login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,23 @@
 const router = require('express').Router();
 const User = require('../models/user');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // registration
 router.post("/register", async (req, res) => {
     //validate the user input (name, email, password)
     const { error } = registerValidation(req.body);
 
-    if(error) {
-        return res.status(400).json({error: error.details[0].message });
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
     }
 
     //check if the email is already registered
-    const emailExist = await User.findOne({email: req.body.email});
+    const emailExist = await User.findOne({ email: req.body.email });
 
     if (emailExist) {
-        return res.status(400).json({error: "Email already exists"});
+        return res.status(400).json({ error: "Email already exists" });
     }
 
     //hash the password
@@ -32,10 +33,10 @@ router.post("/register", async (req, res) => {
 
     try {
         const savedUser = await userObject.save();
-        res.json({error: null, data: savedUser._id});
+        res.json({ error: null, data: savedUser._id });
 
     } catch (error) {
-        res.status(400).json({error})
+        res.status(400).json({ error })
     }
 });
 
@@ -45,16 +46,16 @@ router.post("/login", async (req, res) => {
     // validate user login info
     const { error } = loginValidation(req.body);
 
-    if(error) {
-        return res.status(400).json({error: error.details[0].message });
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
     }
 
     // IF LOGIN INFO IS VALID, FIND THE USER
-    const user = await User.findOne({email: req.body.email});
+    const user = await User.findOne({ email: req.body.email });
 
     // throw error if email is wrong (user does not exit in db)
     if (!user) {
-        return res.status(400).json({error: "Email is wrong"});
+        return res.status(400).json({ error: "Email is wrong" });
     }
 
     // user exit  check for password correctness
@@ -62,10 +63,28 @@ router.post("/login", async (req, res) => {
 
     // throw error if password correctness
     if (!validPassword) {
-        return res.status(400).json({error: "Password is wrong"});
+        return res.status(400).json({ error: "Password is wrong" });
     }
 
-    return res.status(200).json({ msg: "login route..." });
-})
+    //create authentication token with username and id
+    const token = jwt.sign
+        (
+            //payload
+            {
+                name: user.name,
+                id: user._id
+            },
+            //TOKEN_SECRET
+            process.env.TOKEN_SECRET,
+            //EXPIRATION
+            { expiresIn: process.env.JWT_EXPIRES_IN },
+        );
+
+    // attach auth token to header
+    res.header("auth-token", token).json({
+        error: null,
+        data: { token }
+    });
+});
 
 module.exports = router;
