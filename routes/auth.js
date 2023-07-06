c06b2244c4fc1a86b4064485c376869d1ad3cda4
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-07-06 16:15:33 +0700

    Complete register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const user = require('../models/user');
+const User = require('../models/user');
 const { registerValidation } = require('../validation');
+const bcrypt = require('bcrypt');
 
 // registration
 router.post("/register", async (req, res) => {
@@ -10,11 +11,33 @@ router.post("/register", async (req, res) => {
     if(error) {
         return res.status(400).json({error: error.details[0].message });
     }
+
     //check if the email is already registered
+    const emailExist = await User.findOne({email: req.body.email});
+
+    if (emailExist) {
+        return res.status(400).json({error: "Email already exists"});
+    }
+
     //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
     //create a user object and save in the db
-    //return res.status(200).json({ msg: "Register route..." });
-})
+    const userObject = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password
+    });
+
+    try {
+        const savedUser = await userObject.save();
+        res.json({error: null, data: savedUser._id});
+
+    } catch (error) {
+        res.status(400).json({error})
+    }
+});
 
 //login
 router.post("/login", async (req, res) => {
--- validation.js
+++ validation.js
@@ -14,17 +14,3 @@ const registerValidation = (data) => {
 }
 
 module.exports = { registerValidation };
-
-// const joi = require('joi');
-
-// const registerValidation = (data) => {
-//     const schema = joi.object(
-//         {
-//             name: joi.string().min(6).max(255).required(),
-//             email: joi.string().min(6).max(255).required(),
-//             password: joi.string().min(6).max(255).required()
-//         });
-//     return schema.validate(data);
-// }
-
-// module.exports = { registerValidation };
