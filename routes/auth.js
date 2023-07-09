46e0034d1868e098cf3ef7c9a934858e7d7235fa
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2023-07-09 17:03:32 +0700

    use  bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/user');
 const { registerValidation, loginValidation } = require('../validation');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // registration
